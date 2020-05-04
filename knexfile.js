c68c1bdc0ff020a3b5ffb77891b66a8f12186249
lee-wood <leemaewood@gmail.com>
2020-05-04 18:37:09 -0400

    fied typo in knexfile

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
